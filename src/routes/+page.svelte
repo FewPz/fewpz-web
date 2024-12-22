0362e2e510c2d939e672220e6e6acbb7144bbd36
FewPz <sharpkung4826@gmail.com>
2024-12-22 17:10:06 +0700

    feat: replace resume button with link to PDF and open in new tab

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -118,12 +118,13 @@
 						School of Information Technology, KMITL.
 					</p>
 					<div class="flex items-center gap-4">
-						<button
+						<a
 							class="rounded bg-white px-4 py-2 font-semibold text-primary transition-colors hover:bg-slate-100 md:px-6"
-							disabled
+							href="/pdf/cv.pdf"
+							target="_blank"
 						>
 							Resume
-						</button>
+						</a>
 						<div class="flex gap-4">
 							<a
 								href="https://github.com/FewPz"
